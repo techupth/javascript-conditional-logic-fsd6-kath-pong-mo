e0d4fb4def3a9b64279655a6eb04dc91b64f8fac
Pong <pongpong34756@gmail.com>
2024-05-15 14:50:17 +0700

    feat:conditional-logic HW

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,9 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+console.log(
+  lightBulbStatus === "Off"
+    ? "Light bulb is Off."
+    : "Please choose the correct input (On/Off)"
+);
+console.log(lightBulbStatus);
--- ex-4.js
+++ ex-4.js
@@ -1,5 +1,20 @@
 //Exercise 4
 // Switch Statement
 let lightBulbStatus = "On";
-
+let message;
 //Start coding here
+switch (lightBulbStatus) {
+  case "On":
+    console.log("Light bulb is On.");
+    break;
+  case "Off":
+    console.log("Light bulb is Off.");
+    break;
+  case "Broken":
+    console.log("Light bulb is Broken.");
+    break;
+  default:
+    console.log("Please choose the correct input (On/Off/Broken)");
+}
+
+console.log(lightBulbStatus);
